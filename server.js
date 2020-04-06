fedc519ec03f38192de20a8c34384963751cbd37
Emilio Amaya <emilio.a.amaya@vanderbilt.edu>
2020-04-06 18:44:04 -0500

    created more queries and routes

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const app = express();
 const db = require("./server/db");
-const query = require("./server/queries");
+const queries = require("./server/queries");
 
+// Search for businesses by zipcode
 app.get("/api/businesses", (req, res) => {
-  db.query(query.selectByZip(28078), (err, result) => {
+  db.query(queries.selectAll, (err, result) => {
     if (err) throw err;
-    console.log(result);
     res.send(result);
   });
+});
+
+// Query for businesses by zipcode
+app.get("/businesses/zipcode/:zip", (req, res, next) => {
+  try {
+    db.query(queries.selectByZip(req.params.zip), (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get one business by businessId
+app.get("/api/businesses/:businessId", (req, res, next) => {
+  try {
+    db.query(queries.selectOne(req.params.userId), (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 const PORT = process.env.PORT || 5000;
--- server/queries.js
+++ server/queries.js
@@ -1,8 +1,17 @@
 const queries = {
   selectOne: (businessId) =>
-    `SELECT * FROM business WHERE business_id = '${businessId}'`,
+    `SELECT * FROM business b
+    INNER JOIN attributes a ON a.business_id = b.business_id
+    INNER JOIN checkin c ON c.business_id = b.business_id
+    INNER JOIN hours h on h.business_id = b.business_id
+    WHERE b.business_id = '${businessId}';`,
   selectByZip: (zipCode) =>
     `SELECT * FROM business WHERE postal_code = '${zipCode}'`,
+  selectAll: () => `SELECT * FROM business`,
+  selectByCity: (city) => `SELECT * FROM business WHERE city = '${city}'`,
+  selectByState: (state) => `SELECT * FROM business WHERE city = '${state}'`,
+  searchByName: (search) =>
+    `SELECT * FROM business WHERE name LIKE '%${search}%';`,
 };
 
 module.exports = queries;
